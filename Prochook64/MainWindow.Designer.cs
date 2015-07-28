b8f56665fedfff9cd10c8756cc0ccc8d2ac47b1e
William Mortl <will@williammortl.com>
2015-07-28 13:01:30 -0600

    fixed form for auto size and no max box

--- Prochook64/MainWindow.Designer.cs
+++ Prochook64/MainWindow.Designer.cs
@@ -39,48 +39,53 @@
             // 
             // hookText
             // 
-            this.hookText.Location = new System.Drawing.Point(13, 27);
+            this.hookText.Location = new System.Drawing.Point(26, 52);
+            this.hookText.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.hookText.Multiline = true;
             this.hookText.Name = "hookText";
             this.hookText.ReadOnly = true;
             this.hookText.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-            this.hookText.Size = new System.Drawing.Size(553, 168);
+            this.hookText.Size = new System.Drawing.Size(1102, 319);
             this.hookText.TabIndex = 0;
             // 
             // keyText
             // 
-            this.keyText.Location = new System.Drawing.Point(13, 216);
+            this.keyText.Location = new System.Drawing.Point(26, 415);
+            this.keyText.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.keyText.Multiline = true;
             this.keyText.Name = "keyText";
             this.keyText.ReadOnly = true;
             this.keyText.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-            this.keyText.Size = new System.Drawing.Size(553, 168);
+            this.keyText.Size = new System.Drawing.Size(1102, 319);
             this.keyText.TabIndex = 1;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(13, 8);
+            this.label1.Location = new System.Drawing.Point(26, 15);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(80, 13);
+            this.label1.Size = new System.Drawing.Size(157, 25);
             this.label1.TabIndex = 2;
             this.label1.Text = "Prochook Text:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(13, 198);
+            this.label2.Location = new System.Drawing.Point(26, 381);
+            this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(81, 13);
+            this.label2.Size = new System.Drawing.Size(162, 25);
             this.label2.TabIndex = 3;
             this.label2.Text = "Keystroke Text:";
             // 
             // startButton
             // 
             this.startButton.BackColor = System.Drawing.Color.Lime;
-            this.startButton.Location = new System.Drawing.Point(588, 27);
+            this.startButton.Location = new System.Drawing.Point(1162, 52);
+            this.startButton.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.startButton.Name = "startButton";
-            this.startButton.Size = new System.Drawing.Size(128, 101);
+            this.startButton.Size = new System.Drawing.Size(256, 194);
             this.startButton.TabIndex = 4;
             this.startButton.Text = "Start Hooking >>";
             this.startButton.UseVisualStyleBackColor = false;
@@ -89,9 +94,10 @@
             // stopButton
             // 
             this.stopButton.BackColor = System.Drawing.Color.Red;
-            this.stopButton.Location = new System.Drawing.Point(588, 283);
+            this.stopButton.Location = new System.Drawing.Point(1162, 540);
+            this.stopButton.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.stopButton.Name = "stopButton";
-            this.stopButton.Size = new System.Drawing.Size(128, 101);
+            this.stopButton.Size = new System.Drawing.Size(256, 194);
             this.stopButton.TabIndex = 5;
             this.stopButton.Text = "Stop Hooking >>";
             this.stopButton.UseVisualStyleBackColor = false;
@@ -100,9 +106,10 @@
             // getText
             // 
             this.getText.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
-            this.getText.Location = new System.Drawing.Point(588, 154);
+            this.getText.Location = new System.Drawing.Point(1162, 296);
+            this.getText.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.getText.Name = "getText";
-            this.getText.Size = new System.Drawing.Size(128, 101);
+            this.getText.Size = new System.Drawing.Size(256, 194);
             this.getText.TabIndex = 6;
             this.getText.Text = "Get Text >>";
             this.getText.UseVisualStyleBackColor = false;
@@ -110,9 +117,10 @@
             // 
             // MainWindow
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(735, 401);
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(1446, 757);
             this.Controls.Add(this.getText);
             this.Controls.Add(this.stopButton);
             this.Controls.Add(this.startButton);
@@ -120,6 +128,9 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.keyText);
             this.Controls.Add(this.hookText);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.MaximizeBox = false;
             this.Name = "MainWindow";
             this.Text = "64 Bit Prochook Test by William Mortl";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainWindow_FormClosing);
